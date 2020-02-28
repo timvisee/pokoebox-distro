553c1f28bf1c3303237f90a4624acb1fc947b9c3
timvisee <tim@visee.me>
2020-02-28 13:07:36 +0100

    Additional bluetooth audio configuration, improve A2DP sink

--- stage4-pokoe/01-audio/00-run.sh
+++ stage4-pokoe/01-audio/00-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 # Install bluetooth configuration files
+cat files/main.conf.append >> "${ROOTFS_DIR}/etc/bluetooth/main.txt"
 install -m 644 files/audio.conf "${ROOTFS_DIR}/etc/bluetooth/audio.conf"
