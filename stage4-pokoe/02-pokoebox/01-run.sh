99c7f1adff1a7fe359193924a5572e7f0167b3fa
timvisee <tim@visee.me>
2020-03-03 16:36:51 +0100

    Install and configure xscreensaver and HDMI screen blanking

--- stage4-pokoe/02-pokoebox/01-run.sh
+++ stage4-pokoe/02-pokoebox/01-run.sh
@@ -6,3 +6,9 @@ chmod a+x "${ROOTFS_DIR}/opt/pokoebox/update"
 
 # Install other pokoebox files
 install -m 755 files/start "${ROOTFS_DIR}/opt/pokoebox/start"
+
+# Append to config.txt
+cat files/config.txt.append >> "${ROOTFS_DIR}/boot/config.txt"
+
+# Install xscreensaver configuration
+install -m 755 files/xscreensaver "${ROOTFS_DIR}/home/pokoe/.xscreensaver"
