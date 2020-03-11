82d685224a5aee9e20acebda6221d9f6e6134c2f
timvisee <tim@visee.me>
2020-03-11 17:31:21 +0100

    Add systemd service for PulseAudio and MPRIS-proxy

--- stage4-pokoe/02-pokoebox/01-run.sh
+++ stage4-pokoe/02-pokoebox/01-run.sh
@@ -3,6 +3,13 @@
 # Append to config.txt
 cat files/config.txt.append >> "${ROOTFS_DIR}/boot/config.txt"
 
+# Install systemd services
+install -m 644 files/pulseaudio.service "${ROOTFS_DIR}/lib/systemd/user/pulseaudio.service"
+install -m 644 files/mpris-proxy.service "${ROOTFS_DIR}/lib/systemd/user/mpris-proxy.service"
+mkdir -p "${ROOTFS_DIR}/home/pokoe/.config/systemd/user/default.target.wants"
+ln -fs "${ROOTFS_DIR}/lib/systemd/user/pulseaudio.service" "${ROOTFS_DIR}/home/pokoe/.config/systemd/user/default.target.wants/pulseaudio.service"
+ln -fs "${ROOTFS_DIR}/lib/systemd/user/mpris-proxy.service" "${ROOTFS_DIR}/home/pokoe/.config/systemd/user/default.target.wants/mpris-proxy.service"
+
 # Install configuration files
 install -m 755 files/xinitrc "${ROOTFS_DIR}/home/pokoe/.xinitrc"
 install -m 755 files/xscreensaver "${ROOTFS_DIR}/home/pokoe/.xscreensaver"
